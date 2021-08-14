e5b80fb3ae1845fe53beea47020286d211fae6a5
Thiago Hersan <thiago.hersan+github@gmail.com>
2021-08-13 21:49:31 -0400

    adjusts height fill. adds button and localstorage for unique id

---
+++ js/cards.js
@@ -0,0 +1,12 @@
+CARDS = [
+  {
+    id: 0x00,
+    name: 'CARD NAME',
+    text: 'Lal lal lal alskjla kjd lkj.'
+  },
+  {
+    id: 0x01,
+    name: 'OTHER CARD NAME',
+    text: 'BLlakal lal lal alskjla kjd lkj.'
+  }
+]

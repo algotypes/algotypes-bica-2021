88de3990f22803bdb2b3334a95274faa76ee2aac
Thiago Hersan <thiago.hersan+github@gmail.com>
2021-08-18 02:47:08 -0400

    animation logic with localstorage memory

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 // TODO:
-//   - finish draw card logic + animation
-//   - add texts
-//   - 3 cards to choose from
+//   - add text style
 //   - pop up for piramides
 //   - animations/videos on grid spaces
 //     - one spinning pyramid
@@ -27,74 +25,105 @@ function generateId() {
 }
 
 function getId() {
-  const mStorage = window.localStorage;
-  mStorage.setItem('algotypesId', mStorage.getItem('algotypesId') || generateId());
-  return mStorage.getItem('algotypesId');
+  LSTORAGE.setItem('algotypesId', LSTORAGE.getItem('algotypesId') || generateId());
+  return LSTORAGE.getItem('algotypesId');
 }
 
 function intToHexString(i) {
   return `0x${i.toString(16).padStart(2, '0').toUpperCase()}`;
 }
 
+const LSTORAGE = window.localStorage;
 const OVERLAY_TRANSITION_DURATION = 1000;
-const BUTTON_TRANSITION_DURATION = 500;
 const READ_PAUSE = 500;
 const BEAT_PAUSE = 10;
 
-const BUTTON_FADEOUT = 0;
-const BUTTON_HIDE = BUTTON_FADEOUT + BUTTON_TRANSITION_DURATION + BEAT_PAUSE;
-const APP_OVERLAY_FADEIN = BUTTON_HIDE + BEAT_PAUSE;
-const APP_SET_IMAGE = APP_OVERLAY_FADEIN + OVERLAY_TRANSITION_DURATION + READ_PAUSE;
-const APP_OVERLAY_FADEOUT = APP_SET_IMAGE + BEAT_PAUSE;
-const INTRO_OVERLAY_FADEIN = APP_OVERLAY_FADEOUT + OVERLAY_TRANSITION_DURATION + READ_PAUSE;
-const INTRO_REMOVE_IMAGE = INTRO_OVERLAY_FADEIN + OVERLAY_TRANSITION_DURATION + READ_PAUSE;
-const INTRO_OVERLAY_FADEOUT = INTRO_REMOVE_IMAGE + BEAT_PAUSE;
-const MESSAGE_OVERLAY_FADEIN = INTRO_OVERLAY_FADEOUT + OVERLAY_TRANSITION_DURATION + BEAT_PAUSE;
-const MESSAGE_REMOVE_IMAGE = MESSAGE_OVERLAY_FADEIN + OVERLAY_TRANSITION_DURATION + READ_PAUSE;
-const MESSAGE_OVERLAY_FADEOUT = MESSAGE_REMOVE_IMAGE + BEAT_PAUSE;
+var CARD_OVERLAY_FADEIN = 0;
+var CARD_SET_IMAGE = CARD_OVERLAY_FADEIN + OVERLAY_TRANSITION_DURATION + READ_PAUSE;
+var CARD_OVERLAY_FADEOUT = CARD_SET_IMAGE + BEAT_PAUSE;
+var INTRO_OVERLAY_FADEIN = CARD_OVERLAY_FADEOUT + OVERLAY_TRANSITION_DURATION + READ_PAUSE;
+var INTRO_REMOVE_IMAGE = INTRO_OVERLAY_FADEIN + OVERLAY_TRANSITION_DURATION + READ_PAUSE;
+var INTRO_OVERLAY_FADEOUT = INTRO_REMOVE_IMAGE + BEAT_PAUSE;
+var MESSAGE_OVERLAY_FADEIN = INTRO_OVERLAY_FADEOUT + OVERLAY_TRANSITION_DURATION + BEAT_PAUSE;
+var MESSAGE_REMOVE_IMAGE = MESSAGE_OVERLAY_FADEIN + OVERLAY_TRANSITION_DURATION + READ_PAUSE;
+var MESSAGE_OVERLAY_FADEOUT = MESSAGE_REMOVE_IMAGE + BEAT_PAUSE;
+
+function zeroDelays() {
+  CARD_OVERLAY_FADEIN =
+    CARD_SET_IMAGE =
+    CARD_OVERLAY_FADEOUT =
+    INTRO_OVERLAY_FADEIN =
+    INTRO_REMOVE_IMAGE =
+    INTRO_OVERLAY_FADEOUT =
+    MESSAGE_OVERLAY_FADEIN =
+    MESSAGE_REMOVE_IMAGE =
+    MESSAGE_OVERLAY_FADEOUT = 0;
+}
+
+function onCardClick(event) {
+  document.getElementById(event.target.parentElement.id).style.opacity = .8;
+
+  const allCards = Array.from(document.getElementsByClassName('dynamic'));
+  allCards.forEach(c => c.classList.remove('active'));
+  allCards.forEach(c => c.removeEventListener('click', onCardClick));
+
+  const mCardIndex = parseInt(LSTORAGE.getItem('algotypesNextCardIndex'));
+  const mDailyCard = CARDS[mCardIndex];
+  const mCardImageUrl = `imgs/cards/${intToHexString(mDailyCard.number)}.png`;
+  const fullMessage = `${mDailyCard.message.pt}<p>Volte amanhã para uma nova leitura.</p>`;
+
+  const mCardId = event.target.parentElement.id;
+  const mIntroId = (mCardId === 'card-left') ? 'card-center' : 'card-left';
+  const mMessageId = (mCardId === 'card-right') ? 'card-center' : 'card-right';
+  const mCard = document.getElementById(mCardId);
+  const mIntro = document.getElementById(mIntroId);
+  const mMessage = document.getElementById(mMessageId);
+
+  const mCardText = mCard.querySelector('.text');
+  const mIntroText = mIntro.querySelector('.text');
+  const mMessageText = mMessage.querySelector('.text');
+  const mCardOverlay = mCard.querySelector('.overlay');
+  const mIntroOverlay = mIntro.querySelector('.overlay');
+  const mMessageOverlay = mMessage.querySelector('.overlay');
+
+  LSTORAGE.setItem('algotypesLastClickedId', mCardId);
+  LSTORAGE.setItem('algotypesLastCardIndex', mCardIndex);
+
+  // fade card
+  setTimeout(() => mCardOverlay.style.opacity = 1, CARD_OVERLAY_FADEIN);
+  setTimeout(() => mCard.style.backgroundImage = `url(${mCardImageUrl})`, CARD_SET_IMAGE);
+  setTimeout(() => mCardOverlay.style.opacity = 0, CARD_OVERLAY_FADEOUT);
+
+  // fade intro text
+  setTimeout(() => mIntroOverlay.style.opacity = 1, INTRO_OVERLAY_FADEIN);
+  setTimeout(() => mIntro.style.backgroundImage = 'none', INTRO_REMOVE_IMAGE);
+  setTimeout(() => mIntroText.innerHTML = mDailyCard.algorithm.pt, INTRO_REMOVE_IMAGE);
+  setTimeout(() => mIntroOverlay.style.opacity = 0, INTRO_OVERLAY_FADEOUT);
+
+  // fade message text
+  setTimeout(() => mMessageOverlay.style.opacity = 1, MESSAGE_OVERLAY_FADEIN);
+  setTimeout(() => mMessage.style.backgroundImage = 'none', MESSAGE_REMOVE_IMAGE);
+  setTimeout(() => mMessageText.innerHTML = fullMessage, MESSAGE_REMOVE_IMAGE);
+  setTimeout(() => mMessageOverlay.style.opacity = 0, MESSAGE_OVERLAY_FADEOUT);
+}
 
 window.onload = (event) => {
-  const mApp = document.getElementById('my-app');
-  const mIntro = document.getElementById('my-intro');
-  const mMessage = document.getElementById('my-message');
-  const mStartButton = document.getElementById('my-start-button');
-  const mAppOverlay = document.getElementById('my-app-overlay');
-  const mIntroOverlay = document.getElementById('my-intro-overlay');
-  const mMessageOverlay = document.getElementById('my-message-overlay');
-  const mIntroText = document.getElementById('my-intro-text');
-  const mMessageText = document.getElementById('my-message-text');
+  const allCards = Array.from(document.getElementsByClassName('dynamic'));
+  const allOverlay = Array.from(document.getElementsByClassName('overlay'));
 
   const mDailyRandom = dailyRandom(getId() + dayOfYear());
-  const mCard = CARDS[Math.floor(CARDS.length * mDailyRandom)];
-  console.log(mCard);
-  const mCardImageUrl = `imgs/cards/${intToHexString(mCard.number)}.png`;
+  const nextCardIndex = Math.floor(CARDS.length * mDailyRandom);
+  LSTORAGE.setItem('algotypesNextCardIndex', nextCardIndex);
+  const lastCardIndex = parseInt(LSTORAGE.getItem('algotypesLastCardIndex'));
 
-  mStartButton.style.transitionDuration = `${BUTTON_TRANSITION_DURATION / 1000}s`;
-  mAppOverlay.style.transitionDuration = `${OVERLAY_TRANSITION_DURATION / 1000}s`;
-  mIntroOverlay.style.transitionDuration = `${OVERLAY_TRANSITION_DURATION / 1000}s`;
-  mMessageOverlay.style.transitionDuration = `${OVERLAY_TRANSITION_DURATION / 1000}s`;
+  allCards.forEach(c => c.addEventListener('click', onCardClick));
+  allOverlay.forEach(e => e.style.transitionDuration = `${OVERLAY_TRANSITION_DURATION / 1000}s`);
 
-  mStartButton.addEventListener('click', (event) => {
-    // fade button
-    setTimeout(() => mStartButton.style.opacity = 0, BUTTON_FADEOUT);
-    setTimeout(() => mStartButton.style.zIndex = -100, BUTTON_HIDE);
-
-    // fade card
-    setTimeout(() => mAppOverlay.style.opacity = 1, APP_OVERLAY_FADEIN);
-    setTimeout(() => mApp.style.backgroundImage = `url(${mCardImageUrl})`, APP_SET_IMAGE);
-    setTimeout(() => mAppOverlay.style.opacity = 0, APP_OVERLAY_FADEOUT);
-
-    // fade intro text
-    setTimeout(() => mIntroOverlay.style.opacity = 1, INTRO_OVERLAY_FADEIN);
-    setTimeout(() => mIntro.style.backgroundImage = 'none', INTRO_REMOVE_IMAGE);
-    setTimeout(() => mIntroText.innerHTML = mCard.algorithm.pt, INTRO_REMOVE_IMAGE);
-    setTimeout(() => mIntroOverlay.style.opacity = 0, INTRO_OVERLAY_FADEOUT);
-
-    // fade message text
-    const fullMessage = `${mCard.message.pt}<p>Volte amanhã para uma nova leitura.</p>`
-    setTimeout(() => mMessageOverlay.style.opacity = 1, MESSAGE_OVERLAY_FADEIN);
-    setTimeout(() => mMessage.style.backgroundImage = 'none', MESSAGE_REMOVE_IMAGE);
-    setTimeout(() => mMessageText.innerHTML = fullMessage, MESSAGE_REMOVE_IMAGE);
-    setTimeout(() => mMessageOverlay.style.opacity = 0, MESSAGE_OVERLAY_FADEOUT);
-  });
+  if (lastCardIndex === nextCardIndex) {
+    zeroDelays();
+    const lastClickedId = LSTORAGE.getItem('algotypesLastClickedId');
+    document.getElementById(lastClickedId).querySelector('.overlay').click();
+  } else {
+    allCards.forEach(c => c.classList.add('active'));
+  }
 };
